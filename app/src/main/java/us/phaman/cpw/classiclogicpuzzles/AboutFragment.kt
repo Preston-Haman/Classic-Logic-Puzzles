4c2bca7c470bed53589e0bb8e3d622bc89f02edb
Preston H <92552522+Preston-Haman@users.noreply.github.com>
2022-06-09 22:44:01 -0700

    Implement Intent to Open GitHub/Patreon Webpage

--- app/src/main/java/us/phaman/cpw/classiclogicpuzzles/AboutFragment.kt
+++ app/src/main/java/us/phaman/cpw/classiclogicpuzzles/AboutFragment.kt
@@ -1,6 +1,5 @@
 package us.phaman.cpw.classiclogicpuzzles
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -22,11 +21,11 @@ class AboutFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_about, container, false)
 
         view.findViewById<Button>(R.id.about_author_website_button).setOnClickListener {
-            openWebpageInDeviceBrowser(TODO())
+            openWebpageInDeviceBrowser(getString(R.string.github_profile_url))
         }
 
         view.findViewById<Button>(R.id.about_artist_website_button).setOnClickListener {
-            openWebpageInDeviceBrowser(TODO())
+            openWebpageInDeviceBrowser(getString(R.string.patreon_profile_url))
         }
 
         view.findViewById<Button>(R.id.about_back_button).setOnClickListener {
@@ -36,8 +35,7 @@ class AboutFragment : Fragment() {
         return view
     }
 
-    @SuppressLint("QueryPermissionsNeeded")
-    fun openWebpageInDeviceBrowser(url: String) {
+    private fun openWebpageInDeviceBrowser(url: String) {
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
         if (intent.resolveActivity(requireActivity().packageManager) != null) {
